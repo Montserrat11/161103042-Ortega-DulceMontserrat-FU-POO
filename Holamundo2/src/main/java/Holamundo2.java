875d584a3dff7a5714d29670392c5e8b3fab573c
Dulce Montserrat Ortega Martinez <dulce.ortega1142@uppuebla.edu.mx>
2020-05-20 23:33:46 -0500

    Trabajo clase

---
+++ Holamundo2/src/main/java/Holamundo2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author orma_
+ */
+public class Holamundo2 {
+    public static void main(String args[]){
+      System.out.println("Hola");
+      
+      System.out.println("Dulce Montserrat Ortega Martínez");
+    }
+}
---
+++ TipoVariables/src/main/java/TipoVariables.java
@@ -0,0 +1,48 @@
+public class TipoVariables {
+    public static void main(String args[]){
+        //
+        /*
+        FUERTEMENTE TIPADO 
+        Debemos de declarar el tipo de variable que es y esta siempre en el programa
+        
+        una variable no puede pasar facilmente a ser de otro tipo
+        
+        Castear el convertir una variable de un tipo a otro
+        
+        
+        
+        */
+        //tipos numericos
+        int a=10, b=2;
+        
+        //operaciones aritmenticas
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        
+        //tipos flotantes
+        float pi=3.14f;
+        double pi2=3.14;
+        
+        //cambios de variable
+        boolean bandera=true;
+        
+        //Ingresar caracteres
+        char letra='a';
+        String palabra="Puedes poner un mensaje largo";
+        
+        System.out.println(palabra);
+        
+        //concatenar
+        System.out.println(palabra+" y esta es una prueba de ello");
+        
+        
+        System.out.println("Este es el valor de pi"+pi);
+        System.out.println("Este es el valort deñ circulo"+pi*a);
+        System.out.println(pi+a+"este es el valor del circulo");
+        System.out.println("Este es el valor del circulo");
+    }
+}
