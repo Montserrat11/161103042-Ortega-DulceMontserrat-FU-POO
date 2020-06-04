f8f6103195f8ba7932fcef71373c2d132075ccf6
Dulce Montserrat Ortega Martinez <dulce.ortega1142@uppuebla.edu.mx>
2020-06-04 09:39:05 -0500

    Tarjeta1

--- Tarjeta/src/main/java/MainTarjeta.java
+++ Tarjeta/src/main/java/MainTarjeta.java
@@ -1,40 +1,63 @@
-import  java.util.Scanner ;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author orma_
+ */
+import java.util.*;
 public class MainTarjeta {
-    public static void main (String args[]){
-Scanner leer=new Scanner(System.in);
-
-//Avisando al programa que hare un objeto tarjeta;
-Tarjeta tarjeta;
-
-int nCuenta, NIP, opcion;
-System.out.print("Ingrese su numero de cuenta: ");
-nCuenta=leer.nextInt();
-
-System.out.print("Ingrese su numero de NIP: ");
-NIP=leer.nextInt();
-
-//instanciando y llamando al metodo
-tarjeta=new Tarjeta().verificarCuenta(nCuenta, NIP);
-System.out.println(tarjeta.toString());
-
-System.out.println();
-System.out.println();
-
-System.out.println("Tu nombre es el siguiente chavo:"+tarjeta.nombre);
-
-System.out.print("1.- Hacer un deposito");
-System.out.print("\n2.- Hacer un retiro");
-System.out.print("\n3.- Eliminar apartado");
-System.out.print("\n4.- Imprimir datos de la cuenta");
-System.out.print("\n5.- Salir");
-System.out.print("\nElegir una opcion: ");
-opcion=leer.nextInt();
-switch(opcion){
-    case 4:
-        System.out.print(tarjeta.toString());
-        break;
-}
-
-}    
-    
+       public static void main(String args[])
+    {    
+        Tarjeta tarjeta;
+        Scanner scan = new Scanner(System.in);
+        int nCuenta, NIP;
+        int opcion;
+        System.out.print("Número de cuenta: ");
+        nCuenta = scan.nextInt();
+        System.out.print("NIP: ");
+        NIP = scan.nextInt();
+        tarjeta=new Tarjeta().verificarCuenta(nCuenta,NIP);
+        
+        do
+        {
+            System.out.println("¿Qué operación desea realizar?");
+            System.out.println("1.- Depósito");
+            System.out.println("2.- Retiro");
+            System.out.println("3.- Eliminar apartado");
+            System.out.println("4.- Imprimir los datos de su cuenta");
+            System.out.println("5.- Salir");
+            System.out.print("Opción: ");
+            opcion = scan.nextInt();
+            
+            switch(opcion)
+            {
+                case 1:
+                    System.out.print("Monto a depositar: ");
+                    tarjeta.Deposito(scan.nextDouble());
+                    break;
+                    
+                case 2:
+                    System.out.print("Monto a retirar: ");
+                    tarjeta.Retiro(scan.nextDouble());
+                    break;
+                    
+                case 3:
+                    tarjeta.EliminarApartado();
+                    System.out.println("Apartado eliminado");
+                    break;
+                
+                case 4:
+                    System.out.println(tarjeta.toString());
+                    break;
+            }
+        }while(opcion != 5);
+        System.out.println("Puede retirar su Tarjeta");
+       
+        
+        
+    }
 }
--- Tarjeta/src/main/java/Tarjeta.java
+++ Tarjeta/src/main/java/Tarjeta.java
@@ -1,53 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author orma_
+ */
 public class Tarjeta {
-     int nCuenta;
-    int nNip;
+      int nCuenta;
+    int NIP;
     String nombre;
-    int dia;
-    int mes;
-    int anio;
+    int dia, mes, año;
     int cv;
     double monto;
-    double mApartado;
+    double apartado;
     
-    public Tarjeta (){ //constructor vacio
+    public Tarjeta(){}
     
-    }
+    public Tarjeta(int nCuenta, int NIP, String nombre, int dia, int mes, int año, int cv, double monto, double apartado)
+    {
+        this.nCuenta = nCuenta;
+        this.NIP = NIP;
+        this.nombre = nombre;
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+        this.cv = cv;
+        this.monto = monto;
+        this.apartado = apartado;
+        
+}
     
-    public Tarjeta(int nCuenta, int nNip, String nombre, int dia, int mes, int anio, int cv, double monto, double mApartado){
-    this.nCuenta=nCuenta;
-    this.nNip=nNip;
-    this.nombre=nombre;
-    this.dia=dia;
-    this.mes=mes;
-    this.anio=anio;
-    this.cv=cv;
-    this.monto=monto;
-    this.mApartado=mApartado;
-    }
-
-    public Tarjeta verificarCuenta(int nCuenta, int NIP){
-        if(nCuenta == 123123 && NIP == 1133){
-
-            return new Tarjeta(123123, 1133, "Eduardo Diaz Flores", 31, 12, 2032, 543, 360, 45987.22 );
-
+   public Tarjeta verificarCuenta(int nCuenta, int NIP)
+    {
+        if(nCuenta == 123123 && NIP == 1133)
+        {
+            return new Tarjeta(123123, 1133, "Eduardo Díaz Flores",31,12,2032, 543, 45987.22, 550);
         }
-        else if (nCuenta == 456789 && NIP == 3367){
-
-            return new Tarjeta(456789, 3367, "Kevin Meza Gonzales", 31, 12, 2032, 544, 360, 5322.32);
-
+        else if(nCuenta == 456789 && NIP == 3367)
+        {    
+            return new Tarjeta(456789, 3367, "Kevin Meza González",31,12,2032, 544, 360, 5322.23);
         }
-        else 
-            return new Tarjeta();
+        else
+            return new Tarjeta() ;
     }
     
-    public double Total(){
-        return this.monto+this.mApartado;
+    public void Deposito(double monto)
+    {
+        this.monto += monto;
     }
     
+    public void Retiro(double monto){
+            if(monto <=this.monto && monto>=0){
+        
+        this.monto -= monto;
+        
+    }else{
+    
+    System.out.println("tu saldo es insuficiente");
+    }
+        
+ }
+    
+    public void EliminarApartado(){
+        if(monto<=this.monto && monto >=0){
+        this.apartado -= apartado;
+        this.monto=apartado;
+        
+        }else{
+            System.out.println("la cantidad es mayor a tu apartado");
+        }
+        
+    }
+    public double Total()
+    {
+        return this.monto + this.apartado;
+    }
+
     @Override
     public String toString() {
-        return "nCuenta=" + nCuenta + "\n nombre=" + nombre + "\n Fecha de vencimiento=" + dia + ", mes=" + mes + ", anio=" + anio + ", cv=" + cv + ", monto=" + monto + ", mApartado=" + mApartado + '}';
+        return " nCuenta=" + nCuenta + "\n nombre= " + nombre + "\n Fecha de vencimiento" 
+                + dia + "/" + mes + "/" + año + "\n cv= " + cv + "\n monto=" + monto + "\n apartado= " + apartado + "\n Total=" 
+                + this.Total();
     }
     
     
